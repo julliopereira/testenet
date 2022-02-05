a72330323361a23c8df497a979b876a1dcdd8e01
Julio C. Pereira <julliopereira@gmail.com>
2022-02-05 19:15:55 -0300

    Help

---
+++ testenet.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# AUTOR:        Julio C. Pereira
+# APLICATIVO:   Realiza testes para determinado ip de destino
+#               no sentido de identificar se há conectividade
+#               ou/e identificar problemas de qualidade do
+#               serviço.
+#
+# CONTATO:      julliopereira@gmail.com
+#
+# versão: 0.1 - Inicio do desenvolvimento
+#               20220205
+#               Julio C. Pereira
+#
+##########################################################
+# -- HELP --
+msg="
+    Uso $(basename "$0")
+
+        -f, --file          Redireciona para arquivo
+        -m, --mtrace        Faz análise com mtr para o destino
+        -n, --nmap          Faz uma avaliação com nmap -A para o destino
+        -p, --ping          Testa conectividade para o destino
+        -t, --tracert       Faz um tracert para o destino
+        -h, --help          Mostra ajuda do programa
+        -V, --version       Mostra a versão do programa
+
+    Exemplos:
+
+    
+
+"
+
+# -- VARIÁVEIS --
+
+
+# -- FUNÇÕES --
+
+
+# -- CORPO --
+
+
+
+# -- SAÍDA --
+exit 0
